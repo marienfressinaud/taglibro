48f025313438cc1fe91912eed90e9973e4b7858b
Marien Fressinaud <dev@marienfressinaud.fr>
2016-11-16 23:25:25 +0100

    [#6] [tec] Add configuration for Heroku deployment
    Add an entry in config/database.yml in order to be able to configure the
    database.
    Provide configuration to production config to support emails sending.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "taglibro_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'taglibro.org',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
+  config.action_mailer.default_url_options = { host: ENV['HOSTNAME'] }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -83,4 +95,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
 end
